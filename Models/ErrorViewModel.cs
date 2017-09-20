c0979e6394928bf2d96e9a7e6ba437a98ecf7673
fennecfoxsolutions <suporte@fennecfoxsolutions.com.br>
2017-09-20 11:22:45 -0300

    inicio do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RWA.CardosoConsorcio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
