180cec8c46d551af8797f1aa0816b7d482daa345
fennecfoxsolutions <suporte@fennecfoxsolutions.com.br>
2017-09-26 11:37:33 -0300

    banner, separador e vantagens

--- wwwroot/clientapp/components/home/homeController.js
+++ wwwroot/clientapp/components/home/homeController.js
@@ -1,5 +1,29 @@
 app.controller('homeController', function ($scope) {
 
-    $scope.nome = "RWA";
+    $scope.init = function () {
+        
+        //SWIPER BANNER PRINCIPAL
+
+        var swiperBanner = new Swiper('#banner-principal', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: false,
+            autoplay: 8000,
+            effect: 'fade',
+            speed: 1200,
+            autoplayDisableOnInteraction: false,
+            // If we need pagination                        
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev'
+        });        
+        
+    };
+
+    
+    //FUNÇÃO INICIAL
+
+    angular.element(document).ready(function () {
+        $scope.init();
+    });
 
 });
